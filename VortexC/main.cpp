0b732f97385c9348f3638eed26c8ea01a55f22ba
Eddy <eddyc2025@gmail.com>
2026-01-01 17:07:56 -0600

    added timers for producer threads

--- VortexC/main.cpp
+++ VortexC/main.cpp
@@ -4,9 +4,6 @@
 #include <chrono>
 #include <fstream>
 
-
-
-
 typedef struct StreamData {
 	void* ptr;
 	uint64_t streamSizePower;
@@ -19,10 +16,30 @@ DWORD WINAPI produceFirst32Bytes(LPVOID parameters) {
 	uint64_t AVXperBlock = 1ULL << 21 >> 5;
 	__m256i x = _mm256_set1_epi32(1);
 	
+	uint64_t steps = 0;
+	uint64_t interval = 1 << 10;
+	uint64_t bytesPerGigabyte = pow(10, 9);
+	clock_t lastClock = clock();
+	clock_t startClock = lastClock;
 	for (uint64_t i = 0; i < lenBlocks; i++) {
 		__m256i* p = (__m256i*)bufWptr;
 		_mm256_store_si256(p + i * AVXperBlock, x);
+
+		if (++steps > interval) {
+			clock_t curClock = clock();
+			double delay = ((double)curClock - lastClock) / CLOCKS_PER_SEC;
+			if (curClock - lastClock == 0) {
+				delay = 0.001;
+			}
+			interval /= delay;
+			std::cout << "Progress : " << (double)((i + 1) << 21) / bytesPerGigabyte << " / " << (double)(1ULL << streamSizePower) / bytesPerGigabyte << "GB\n";
+			std::cout << "Total Elapsed Time : " << ((double)curClock - startClock) / CLOCKS_PER_SEC << "seconds\n";
+			lastClock = curClock;
+			steps = 0;
+		}
 	}
+	std::cout << "Progress : " << (double)(1ULL << streamSizePower) / bytesPerGigabyte << " / " << (double)(1ULL << streamSizePower) / bytesPerGigabyte << "GB\n";
+	std::cout << "Total Elapsed Time : " << ((double)clock() - startClock) / CLOCKS_PER_SEC << "seconds\n";
 	return ERROR_SUCCESS;
 }
 
@@ -31,12 +48,32 @@ DWORD WINAPI produceEntireBuffer(LPVOID parameters) {
 	uint64_t streamSizePower = ((StreamData*)parameters)->streamSizePower;
 	uint64_t lenAVX = 1ULL << streamSizePower >> 5;
 	__m256i x = _mm256_set1_epi32(1);
-	auto ref = std::chrono::steady_clock::now();
 
+	uint64_t steps = 0;
+	uint64_t interval = 1 << 10;
+	uint64_t bytesPerGigabyte = pow(10, 9);
+	clock_t lastClock = clock();
+	clock_t startClock = lastClock;
+	
 	for (uint64_t i = 0; i < lenAVX; i++) {
 		__m256i* p = (__m256i*)bufWptr;
 		_mm256_store_si256(p + i, x);
+
+		if (++steps > interval) {
+			clock_t curClock = clock();
+			double delay = ((double)curClock - lastClock) / CLOCKS_PER_SEC;
+			if (curClock - lastClock == 0) {
+				delay = 0.001;
+			}
+			interval /= delay;
+			std::cout << "Progress : " << (double)((i + 1) << 5) / bytesPerGigabyte << " / " << (double)(1ULL << streamSizePower) / bytesPerGigabyte << "GB\n";
+			std::cout << "Total Elapsed Time : " << ((double)curClock - startClock) / CLOCKS_PER_SEC << "seconds\n";
+			lastClock = curClock;
+			steps = 0;
+		}
 	}
+	std::cout << "Progress : " << (double)(1ULL << streamSizePower) / bytesPerGigabyte << " / " << (double)(1ULL << streamSizePower) / bytesPerGigabyte << "GB\n";
+	std::cout << "Total Elapsed Time : " << ((double)clock() - startClock) / CLOCKS_PER_SEC << "seconds\n";
 	return ERROR_SUCCESS;
 }
 
@@ -53,7 +90,7 @@ DWORD WINAPI consumeFirst32Bytes(LPVOID parameters) {
 		sum = _mm256_add_epi32(sum, x);
 	}
 	if (sum.m256i_i32[0] < 2) {
-		std::cout << "This is here to prevent the optimizations for release mode from optimizing away this entire function.";
+		std::cout << "This is here to prevent the optimizations for release mode from optimizing away this entire function.\n";
 	}
 	return ERROR_SUCCESS;
 }
@@ -70,7 +107,7 @@ DWORD WINAPI consumeEntireBuffer(LPVOID parameters) {
 		sum = _mm256_add_epi32(sum, x);
 	}
 	if (sum.m256i_i32[0] < 2) {
-		std::cout << "This is here to prevent the optimizations for release mode from optimizing away this entire function.";
+		std::cout << "This is here to prevent the optimizations for release mode from optimizing away this entire function.\n";
 	}
 	return ERROR_SUCCESS;
 }
@@ -93,10 +130,11 @@ void waitForThread(HANDLE thread) {
 }
 
 int main() {
+	std::ios::sync_with_stdio(false);
 	uint64_t streamSizePower, blockSizePower;
 	unsigned int L, M, N;
 
-	streamSizePower = 30;
+	streamSizePower = 37;
 	blockSizePower = 21;
 	L = 1, M = 0, N = 2;
 	
@@ -107,7 +145,7 @@ int main() {
 	StreamData consumeParam = { bufR, streamSizePower };
 	
 	double streamSizeDecimalGB = (1ULL << streamSizePower) / pow(10, 9);
-	unsigned int numTests = 10;
+	unsigned int numTests = 3;
 	for (unsigned int i = 1; i < numTests + 1; i++) {
 		std::cout << "test " << i << " beginning...\n";
 		clock_t begin = clock();
